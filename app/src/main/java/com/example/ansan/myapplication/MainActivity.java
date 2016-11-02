0b215350d8aab4c8d2b4e881a496dc32f2bdd94b
chomina <alsk8368@naver.com>
2016-11-02 12:44:26 +0900

    SD card 폴더 생성/ 삭제 기능 추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.ansan.myapplication;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +24,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClick(View v){
+        if (isStoragePermissionGranted() == false){
+            Toast.makeText(getApplicationContext(),"SD Card 사용 불가",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String folder =path + "/myLoveDir";
+        String filename =folder + "/myfile.txt";
+
+        File myfolder = new File(folder);
+
         switch(v.getId()){
             case R.id.button:   //폴더 생성
+                myfolder.mkdir();
+                Toast.makeText(getApplicationContext(),"폴더 생성",Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.button2:  //폴더 삭제
+                myfolder.delete();
+                Toast.makeText(getApplicationContext(),"폴더 삭제",Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.button3:  //파일 생성
+
+                Toast.makeText(getApplicationContext(),"파일 생성",Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.button4:  //파일 읽기
+
+                Toast.makeText(getApplicationContext(),"파일 읽기",Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.button5:  //파일 목록 가져오기
+                myfolder.mkdir();
+                Toast.makeText(getApplicationContext(),"파일 목록 가져오기",Toast.LENGTH_SHORT).show();
                 break;
 
         }
